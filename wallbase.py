5161204ffff845673b6c8ca8b3e39b3f0bd67dad
Refrax <refrax@gmail.com>
2014-06-23 11:49:20 -0500

    made more PEP8 compliant

--- wallbase.py
+++ wallbase.py
@@ -7,22 +7,23 @@ import urllib.request
 from bs4 import BeautifulSoup
 import hashlib
 
+
 def get_pic(number_imgs, timespan):
     """Returns a list of the specified number of top images over the specified
     timeframe from wallbase.cc
     """
     url = "http://wallbase.cc/toplist"
     opts = {
-        'section': 'wallpapers', 'q': '', 'res_opt': 'gteq', 
-        'res': '1920x1080', 'aspect': '1.77', 'purity': '100', 
+        'section': 'wallpapers', 'q': '', 'res_opt': 'gteq',
+        'res': '1920x1080', 'aspect': '1.77', 'purity': '100',
         'board': '21', 'thpp': number_imgs, 'ts': timespan
     }
-    htmltext = requests.get(url, params = opts)
+    htmltext = requests.get(url, params=opts)
     page_urls = []
     img_urls = []
     soup = BeautifulSoup(htmltext.content)
     results = soup.findAll("a")
-    
+
     for r in results:
         if "http://wallbase.cc/wallpaper/" in r['href']:
             page_urls.append(r['href'])
@@ -37,6 +38,7 @@ def get_pic(number_imgs, timespan):
 
     return img_urls
 
+
 def save_pic(url, save_path):
     """Saves a file to disk when given a URL"""
     hs = hashlib.md5(url.encode('UTF-8')).hexdigest()
@@ -46,7 +48,7 @@ def save_pic(url, save_path):
     if os.path.isfile(to_save):
         print(file_name + "\texists, skipping...")
     else:
-        print(file_name+ "\tdownloading...")
+        print(file_name + "\tdownloading...")
         urllib.request.urlretrieve(url, to_save)
 
 if __name__ == "__main__":
